5a3c3d6138ba5cc1cb63aa29352d27cd11c88182
Sonny Tambiac <baguiosfines007@gmail.com>
2019-08-06 16:29:35 +1000

    Updates

---
+++ precache-manifest.a9b28d5551983065318f1ad562211701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231ddcd422723e6aacfbef7d9d1687f9",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8b62c2799a164c13ce25",
+    "url": "/resume/static/css/main.d616a04d.chunk.css"
+  },
+  {
+    "revision": "02c9c73ced59eeb46bae",
+    "url": "/resume/static/js/2.bd00f70e.chunk.js"
+  },
+  {
+    "revision": "8b62c2799a164c13ce25",
+    "url": "/resume/static/js/main.3c11b73f.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "930f91f3bfb1f345c3a0572155a28733",
+    "url": "/resume/static/media/edit-document.930f91f3.svg"
+  },
+  {
+    "revision": "ff5e955e1023ea8af91d1b1c002537dd",
+    "url": "/resume/static/media/galaxy.ff5e955e.jpg"
+  }
+]);
